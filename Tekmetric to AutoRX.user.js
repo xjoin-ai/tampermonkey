78d33a0e8516c4cd915724685b6a6b1ac199e98f
Chase Stice <chaserstice@hotmail.com>
2024-05-02 22:48:57 -0600

    Setting the button in the header

--- Tekmetric to AutoRX.user.js
+++ Tekmetric to AutoRX.user.js
@@ -11,21 +11,25 @@
 
 function submit_search() {
     'use strict';
-
-    const buildEst = Array.from(document.querySelectorAll('[data-cy="build-estimate-primary"]'))[0];
-    const buildClone = buildEst.cloneNode(true);
-    buildClone.setAttribute('id','autorx_button');
-    buildEst.parentNode.parentNode.appendChild(buildClone);
-    buildClone.querySelector('span').lastChild.textContent = "   AutoRx"
-    buildClone.addEventListener("click", alert_message);
-    clearInterval(interval);
+    if(!document.getElementsByClassName('pendo-vehicle-odometer')[0].children[1].outerHTML.toString().includes('Adding')){
+        const mainBody = document.getElementsByTagName('main');
+        const innerdiv = document.createElement('div');
+        innerdiv.innerHTML = 'AutoRx';
+        const autorxButton = document.createElement('button');
+        const header = document.getElementsByTagName('header');
+        autorxButton.setAttribute('class','MuiButtonBase-root outline-none items-center flex text-label-normal cursor-pointer rounded-lg px-4 h-10 bg-transparent disabled:text-v2-shade-200 hover:bg-blue-300');
+        autorxButton.setAttribute('id','autorx_button');
+        autorxButton.addEventListener('click', alert_message);
+        autorxButton.appendChild(innerdiv);
+        document.querySelectorAll('header div')[0].children[0].appendChild(autorxButton);
+        clearInterval(interval);
+    }
 };
 
 function alert_message() {
     const ro = document.getElementsByTagName('title')[0].innerHTML.match('#([0-9]*):')[1]
-    // const url = `https://autorx.xjoin.io/?ro=${ro}&mi=15000&test=false`
-    // const url = `https://autorx.xjoin.io/?ro=1021112&mi=15000&test=false`
-    const url = `https://dev-autorx.xjoin.io/?ro=1021112&mi=15000&test=false`
+    const url = `https://autorx.xjoin.io/?ro=${ro}&mi=15000&test=false`
+    // const url = `https://dev-autorx.xjoin.io/?ro=1021112&mi=15000&test=false`
     window.open(url, '_blank').focus();
 }
 
